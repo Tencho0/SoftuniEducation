96c6f1f54bbd22d884d5317529fd81f485d3bdc2
Tencho0 <tencho1011@gmail.com>
2022-03-13 11:12:34 +0200

    solution to RegularExpressions-Lab

--- C# fund/fund c# jan 22/RegularExpressions-Lab/T02.MatchPhoneNumber/Program.cs
+++ C# fund/fund c# jan 22/RegularExpressions-Lab/T02.MatchPhoneNumber/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace T02.MatchPhoneNumber
 {
@@ -6,7 +8,21 @@ namespace T02.MatchPhoneNumber
     {
         static void Main(string[] args)
         {
+           // Regex regex = new Regex(@"[^ ]?\+359([ -])\d{1}(\1)\d{3}(\1)\d{4}\b");
+            //string phoneNumbers = Console.ReadLine();
+            //MatchCollection matches = regex.Matches(phoneNumbers);
+            //var phoneMatches = matches
+            //    .Cast<Match>()
+            //    .Select(x => x.Value.Trim())
+            //    .ToArray();
 
+          //  var matches = Regex.Matches(Console.ReadLine(), @"(\+ ?359([ -])2(\2)\d{3}(\2)\d{4}\b)"); 
+            var matches = Regex.Matches(Console.ReadLine(), @"[^ ]?\+359([ -])\d{1}(\1)\d{3}(\1)\d{4}\b");       
+            var phoneMatches = matches
+                .Cast<Match>()
+                .Select(x => x.Value.Trim())
+                .ToArray();
+            Console.WriteLine(string.Join(", ", phoneMatches));
         }
     }
 }
