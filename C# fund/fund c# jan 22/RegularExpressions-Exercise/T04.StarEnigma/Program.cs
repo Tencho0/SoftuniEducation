e4344ef48dc23848406fee290017124b47503f45
Tencho0 <tencho1011@gmail.com>
2022-03-19 21:59:58 +0200

    Task to RegularExpressions-Exercises

--- C# fund/fund c# jan 22/RegularExpressions-Exercise/T04.StarEnigma/Program.cs
+++ C# fund/fund c# jan 22/RegularExpressions-Exercise/T04.StarEnigma/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace T04.StarEnigma
 {
@@ -6,7 +10,65 @@ namespace T04.StarEnigma
     {
         static void Main(string[] args)
         {
+            List<string> attackPlanets = new List<string>();
+            List<string> destroyedPlanets = new List<string>();
+            string pattern = @"@(?<planet>[A-Za-z]+)[^@\-!:>]*?:(?<population>[\d]+)[^@\-!:>]*?!(?<attackType>[A|D])![^@\-!:>]*?->(?<soldiers>\d+)";
 
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string encrypted = Console.ReadLine();
+                string decrypted = DecryptMessage(encrypted);
+                Match match = Regex.Match(decrypted, pattern);
+                if (match.Success)
+                {
+                    string planet = match.Groups["planet"].Value;
+                    int population = int.Parse(match.Groups["population"].Value);
+                    string attackType = match.Groups["attackType"].Value;
+                    int soldiers = int.Parse(match.Groups["soldiers"].Value);
+                    if (attackType == "A")
+                    {
+                        attackPlanets.Add(planet);
+                    }
+                    else
+                    {
+                        destroyedPlanets.Add(planet);
+                    }
+                }
+            }
+            PrintResult(attackPlanets, destroyedPlanets);
+        }
+
+        private static void PrintResult(List<string> attackPlanets, List<string> destroyedPlanets)
+        {
+            Console.WriteLine($"Attacked planets: {attackPlanets.Count}");
+            foreach (var currPlanet in attackPlanets.OrderBy(x => x))
+            {
+                Console.WriteLine($"-> {currPlanet}");
+            }
+            Console.WriteLine($"Destroyed planets: {destroyedPlanets.Count}");
+            foreach (var currPlanet in destroyedPlanets.OrderBy(x => x))
+            {
+                Console.WriteLine($"-> {currPlanet}");
+            }
+        }
+
+        static string DecryptMessage(string encrypted)
+        {
+            int count = TakeCount(encrypted);
+            StringBuilder sb = new StringBuilder();
+            foreach (char currCh in encrypted)
+            {
+                char currLetter = (char)(currCh - count);
+                sb.Append(currLetter);
+            }
+            return sb.ToString();
+        }
+        static int TakeCount(string encrypted)
+        {
+            string pattern = @"[star]";
+            MatchCollection matches = Regex.Matches(encrypted, pattern, RegexOptions.IgnoreCase);
+            return matches.Count;
         }
     }
 }
---
+++ C# fund/fund c# jan 22/RegularExpressions-Exercise/T05.NetherRealms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T05.NetherRealms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string lettersPattern = @"[A-Za-z]";
+            string digitsPattern = @"[+-]*\d+([\.\d]*)";
+
+            MatchCollection letters = Regex.Matches(input, lettersPattern);
+            MatchCollection digits = Regex.Matches(input, digitsPattern);
+
+        }
+    }
+}
