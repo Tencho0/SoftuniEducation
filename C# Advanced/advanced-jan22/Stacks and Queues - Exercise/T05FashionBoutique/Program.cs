983164fc287d8619c544875def2f813934718605
Tencho0 <tencho1011@gmail.com>
2022-03-27 16:08:23 +0300

    added task in Stacks and Queues-Exercise

--- C# Advanced/advanced-jan22/Stacks and Queues - Exercise/T05FashionBoutique/Program.cs
+++ C# Advanced/advanced-jan22/Stacks and Queues - Exercise/T05FashionBoutique/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace T05FashionBoutique
 {
@@ -6,7 +8,27 @@ namespace T05FashionBoutique
     {
         static void Main(string[] args)
         {
+            int[] inputClothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
 
+            Stack<int> clothes = new Stack<int>(inputClothes);
+            int racks = 1;
+            int countOfClothes = 0;
+
+            while (clothes.Count > 0)
+            {
+                countOfClothes += clothes.Peek();
+                if (countOfClothes <= capacity)
+                {
+                    clothes.Pop();
+                }
+                else
+                {
+                    racks++;
+                    countOfClothes = 0;
+                }
+            }
+            Console.WriteLine(racks);
         }
     }
 }
