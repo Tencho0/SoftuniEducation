5ec927993564a3101313403fe5d2fcdf1f57420e
Tencho0 <tencho1011@gmail.com>
2022-04-02 19:53:40 +0300

    added new final exam - 05

---
+++ C# fund/Fund-Exams-lastYear/05.ProgrammingFundamentalsFinalExam3/T01.ActivationKeys/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace T01.ActivationKeys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = Console.ReadLine();
+            string command = Console.ReadLine();
+            while (command != "Generate")
+            {
+                string[] givenCmd = command
+                    .Split(">>>", StringSplitOptions.RemoveEmptyEntries);
+                string action = givenCmd[0];
+                if (action == "Contains")
+                {
+                    string substring = givenCmd[1];
+                    if (key.Contains(substring))
+                    {
+                        Console.WriteLine($"{key} contains {substring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Substring not found!");
+                    }
+                }
+                else if (action == "Flip")
+                {
+                    string upperOrLower = givenCmd[1];
+                    int startIndex = int.Parse(givenCmd[2]);
+                    int endIndex = int.Parse(givenCmd[3]);
+                    string substring = key.Substring(startIndex, endIndex - startIndex);
+                    key = key.Remove(startIndex, endIndex - startIndex);
+                    if (upperOrLower == "Upper")
+                    {
+                        substring = substring.ToUpper();
+                    }
+                    else if (upperOrLower == "Lower")
+                    {
+                        substring = substring.ToLower();
+                    }
+                    key = key.Insert(startIndex, substring);
+                    Console.WriteLine(key);
+                }
+                else if (action == "Slice")
+                {
+                    int startIndex = int.Parse(givenCmd[1]);
+                    int endIndex = int.Parse(givenCmd[2]);
+                    key = key.Remove(startIndex, endIndex - startIndex);
+                    Console.WriteLine(key);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Your activation key is: {key}");
+        }
+    }
+}
---
+++ C# fund/Fund-Exams-lastYear/05.ProgrammingFundamentalsFinalExam3/T02.EmojiDetector/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T02.EmojiDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string digitsPattern = @"[\d]";
+            string pattern = @"([\:]{2}|[*]{2})(?<emoji>[A-Z][a-z]{2,})\1";
+            string input = Console.ReadLine();
+            long threshold = 1;
+            MatchCollection digits = Regex.Matches(input, digitsPattern);
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match digit in digits)
+            {
+                threshold *= int.Parse(digit.Value);
+            }
+            if (digits.Count == 0)
+            {
+                threshold = 0;
+            }
+            Console.WriteLine($"Cool threshold: {threshold}");
+            Console.WriteLine($"{matches.Count} emojis found in the text. The cool ones are:");
+            foreach (Match match in matches)
+            {
+                long coolnes = 0;
+                foreach (char letter in match.Groups["emoji"].Value)
+                {
+                    coolnes += letter;
+                }
+                if (coolnes >= threshold)
+                {
+                    Console.WriteLine(match);
+                }
+            }
+        }
+    }
+}
