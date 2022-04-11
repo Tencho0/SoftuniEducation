4ff58b2ebf96eaddfd1f08b0e16102a26b656862
Tencho0 <tencho1011@gmail.com>
2022-04-11 12:21:57 +0300

    Created new solution folder - IteratorsAndComparators with 1 finished task

---
+++ C# Advanced/advanced-jan22/IteratorsandComparators-Lab/LibraryIteratorLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryIteratorLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
