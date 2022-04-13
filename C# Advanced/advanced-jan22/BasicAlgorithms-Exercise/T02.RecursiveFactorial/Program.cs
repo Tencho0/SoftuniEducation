ba979c5ddfaec58fb637324b2d7d09fd88e9e627
Tencho0 <tencho1011@gmail.com>
2022-04-13 23:15:44 +0300

    created new solution - basic algorithms - exercise

---
+++ C# Advanced/advanced-jan22/BasicAlgorithms-Exercise/T02.RecursiveFactorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T02.RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());  
+            Console.WriteLine(GetSum(n));
+        }
+
+        private static long GetSum(long n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * GetSum(n - 1);
+        }
+    }
+}
---
+++ C# Advanced/advanced-jan22/BasicAlgorithms-Exercise/Ш01.RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace T01.RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(GetSum(n, 0));
+        }
+
+        private static int GetSum(int[] arr, int index)
+        {
+            int sum = 0;
+            if (index == arr.Length - 1)
+            {
+                return arr[index];
+            }
+            sum += arr[index] + GetSum(arr, index + 1);
+            return sum;
+        }
+    }
+}
