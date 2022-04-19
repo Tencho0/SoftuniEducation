0b07b3f2a8a01e33ff1774b1f0bf3290cb07a8cf
Tencho0 <tencho1011@gmail.com>
2022-04-19 22:00:01 +0300

    added new tasks in solution Inheritance-Exercise

---
+++ C# OOP/C# OOP jan 22/Inheritance-Exercise/T06.Animals/Kitten.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Animals
+{
+    public class Kitten : Cat
+    {
+        public Kitten(string name, int age)
+            : base(name, age, "Female")
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+    }
+}
--- C# OOP/C# OOP jan 22/Inheritance-Exercise/T06.Animals/StartUp.cs
+++ C# OOP/C# OOP jan 22/Inheritance-Exercise/T06.Animals/StartUp.cs
@@ -1,10 +1,59 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Beast!")
+                {
+                    break;
+                }
+                string[] animalData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                
+                string name = animalData[0];
+                int age = int.Parse(animalData[1]);
 
+                if (age < 0)
+                {
+                    Console.WriteLine("Invalid input!");
+                    continue;
+                }
+
+                Animal animal = default;
+                if (command == "Dog")
+                {
+                    string gender = animalData[2];
+                    animal = new Dog(name, age, gender);
+                }
+                else if (command == "Cat")
+                {
+                    string gender = animalData[2];
+                    animal = new Cat(name, age, gender);
+                }
+                else if (command == "Frog")
+                {
+                    string gender = animalData[2];
+                    animal = new Frog(name, age, gender);
+                }
+                else if (command == "Kitten")
+                {
+                    animal = new Kitten(name, age);
+                }
+                else if (command == "Tomcat")
+                {
+                    animal = new Tomcat(name, age);
+                }
+
+                Console.WriteLine($"{command}");
+                Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                Console.WriteLine(animal.ProduceSound());
+            }
         }
     }
 }
