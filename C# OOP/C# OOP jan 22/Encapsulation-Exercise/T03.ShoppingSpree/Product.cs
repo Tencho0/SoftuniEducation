0b91c586f11cf06f3358332201f89e40e63a680c
Tencho0 <tencho1011@gmail.com>
2022-04-20 13:07:43 +0300

    Added new solution - Encapsulation-Exercise

---
+++ C# OOP/C# OOP jan 22/Encapsulation-Exercise/T03.ShoppingSpree/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T03.ShoppingSpree
+{
+    public class Product
+    {
+        private string name;
+        private decimal cost;
+        public Product(string name, decimal cost)
+        {
+            this.Name = name;
+            this.Cost = cost;
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        public decimal Cost
+        {
+            get { return cost; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Money cannot be negative");
+                }
+                cost = value;
+            }
+        }
+    }
+}
---
+++ C# OOP/C# OOP jan 22/Encapsulation-Exercise/T03.ShoppingSpree/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T03.ShoppingSpree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Person> people = new Dictionary<string, Person>();
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+            try
+            {
+                string[] givenPeople = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+                foreach (string currPerson in givenPeople)
+                {
+                    string[] data = currPerson.Split('=', StringSplitOptions.RemoveEmptyEntries);
+                    string name = data[0];
+                    decimal money = decimal.Parse(data[1]);
+                    Person person = new Person(name, money);
+                    people[name] = person;
+                }
+
+                string[] givenProducts = Console.ReadLine()
+                    .Split(';', StringSplitOptions.RemoveEmptyEntries);
+                foreach (string currProduct in givenProducts)
+                {
+                    string[] data = currProduct.Split('=', StringSplitOptions.RemoveEmptyEntries);
+                    string name = data[0];
+                    decimal price = decimal.Parse(data[1]);
+                    Product person = new Product(name, price);
+                    products[name] = person;
+                }
+
+                string command = Console.ReadLine();
+                while (command != "END")
+                {
+                    string[] givenCmd = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    string personName = givenCmd[0];
+                    string productName = givenCmd[1];
+
+                    Person person = people[personName];
+                    Product product = products[productName];
+
+                    if (person.Money < product.Cost)
+                    {
+                        Console.WriteLine($"{personName} can't afford {productName}");
+                    }
+                    else
+                    {
+                        person.Money -= product.Cost;
+                        person.BagOfProducts.Add(product);
+                        Console.WriteLine($"{person.Name} bought {product.Name}");
+                    }
+
+                    command = Console.ReadLine();
+                }
+
+                foreach (var person in people)
+                {
+                    if (person.Value.BagOfProducts.Count == 0)
+                    {
+                        Console.WriteLine($"{person.Key} - Nothing bought");
+                        continue;
+                    }
+
+                    Console.WriteLine($"{person.Key} - {string.Join(", ", person.Value.BagOfProducts.Select(x => x.Name))}");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
