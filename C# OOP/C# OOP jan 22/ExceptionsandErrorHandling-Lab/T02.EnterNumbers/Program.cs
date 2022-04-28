685ea31ee9245d960d8942e08692fb1ccdf78922
Tencho0 <tencho1011@gmail.com>
2022-04-28 22:14:24 +0300

    created new solution - ExceptionsandErrorHandling-Lab

---
+++ C# OOP/C# OOP jan 22/ExceptionsandErrorHandling-Lab/T02.EnterNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T02.EnterNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>();
+            int n = 1;
+            while (nums.Count != 10)
+            {
+                try
+                {
+                    ReadNumber(1, 100);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Your number is not in range {n} - 100!");
+                }
+                n++;
+            }
+        }
+
+        public static void ReadNumber(int start, int end)
+        {
+
+        }
+    }
+}
