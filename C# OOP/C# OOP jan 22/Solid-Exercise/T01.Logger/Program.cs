d310d7afd140bf7868d1f95be2769bf0660f08fd
Tencho0 <tencho1011@gmail.com>
2022-05-06 23:07:10 +0300

    created new solution - Solid-Exercise

---
+++ C# OOP/C# OOP jan 22/Solid-Exercise/T01.Logger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T01.Logger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ILayout simpleLayout = new SimpleLayout();
+            IAppender consoleAppender = new ConsoleAppender(simpleLayout);
+            ILogger logger = new Logger(consoleAppender);
+
+            logger.Error("3/26/2015 2:08:11 PM", "Error parsing JSON.");
+            logger.Info("3/26/2015 2:08:11 PM", "User Pesho successfully registered.");
+
+        }
+    }
+}
