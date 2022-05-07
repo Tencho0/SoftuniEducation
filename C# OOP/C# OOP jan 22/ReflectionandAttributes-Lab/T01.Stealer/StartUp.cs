a18d8f3903f87f1b85700a36bdae255b62b50993
Tencho0 <tencho1011@gmail.com>
2022-05-07 22:10:58 +0300

    created new solution - ReflectionandAttributes-Lab

---
+++ C# OOP/C# OOP jan 22/ReflectionandAttributes-Lab/T01.Stealer/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
