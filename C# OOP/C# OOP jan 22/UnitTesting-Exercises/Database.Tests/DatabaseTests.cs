bce46acaa54d11ae05f5dde40522948034e2bdf1
Tencho0 <tencho1011@gmail.com>
2022-05-12 23:38:05 +0300

    created new solution - UnitTesting-Exercises

--- C# OOP/C# OOP jan 22/UnitTesting-Exercises/Database.Tests/DatabaseTests.cs
+++ C# OOP/C# OOP jan 22/UnitTesting-Exercises/Database.Tests/DatabaseTests.cs
@@ -5,6 +5,28 @@ namespace Database.Tests
     [TestFixture]
     public class DatabaseTests
     {
+        [TestCase(new int[] { 1 })]
+        [TestCase(new int[] { 4, 18, 17, 5, 3, 1000 })]
+        [TestCase(new int[] { int.MinValue, int.MaxValue, 5 })]
+        [TestCase(new int[0])]
+        public void Constructor_With_Validdata_Should_Pass(int[] parameters)
+        {
+            Database database = new Database(parameters);
+            Assert.AreEqual(parameters.Length, database.Count);
+        }
 
+        [TestCase(new int[] { 1, 2 }, new int[] { 10, 15 }, 4)]
+        [TestCase(new int[0], new int[] { int.MinValue, int.MaxValue, 334455}, 3)]
+        public void Add_With_Validdata_Should_Pass(int[] ctorParams, int[] paramsToAdd, int expectedCount)
+        {
+            Database database = new Database(ctorParams);
+
+            for (int i = 0; i < paramsToAdd.Length; i++)
+            {
+                database.Add(paramsToAdd[i]);
+            }
+
+            Assert.AreEqual(expectedCount, database.Count);
+        }
     }
 }
