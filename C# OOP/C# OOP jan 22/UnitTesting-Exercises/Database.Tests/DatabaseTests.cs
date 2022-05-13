614a74fee10aef083c278803094e61122a0b63cc
Tencho0 <tencho1011@gmail.com>
2022-05-13 23:12:05 +0300

    added changes to a project in solution UnitTesting-Exercises

--- C# OOP/C# OOP jan 22/UnitTesting-Exercises/Database.Tests/DatabaseTests.cs
+++ C# OOP/C# OOP jan 22/UnitTesting-Exercises/Database.Tests/DatabaseTests.cs
@@ -1,6 +1,7 @@
 namespace Database.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class DatabaseTests
@@ -16,14 +17,52 @@ namespace Database.Tests
         }
 
         [TestCase(new int[] { 1, 2 }, new int[] { 10, 15 }, 4)]
-        [TestCase(new int[0], new int[] { int.MinValue, int.MaxValue, 334455}, 3)]
-        public void Add_With_Validdata_Should_Pass(int[] ctorParams, int[] paramsToAdd, int expectedCount)
+        [TestCase(new int[0], new int[] { int.MinValue, int.MaxValue, 334455 }, 3)]
+        [TestCase(new int[0], new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 }, 16)]
+        public void Add_With_Validdata_Positive_Test(int[] ctorParams, int[] paramsToAdd, int expectedCount)
         {
             Database database = new Database(ctorParams);
 
             for (int i = 0; i < paramsToAdd.Length; i++)
             {
                 database.Add(paramsToAdd[i]);
+            }
+
+            Assert.AreEqual(expectedCount, database.Count);
+        }
+
+        [TestCase(new int[] { 10, 10 }, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 }, 1)]
+        [TestCase(new int[0], new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 }, 1)]
+        public void Add_With_Validdata_Negative_Test(int[] ctorParams, int[] paramsToAdd, int errorParam)
+        {
+            Database database = new Database(ctorParams);
+
+            for (int i = 0; i < paramsToAdd.Length; i++)
+            {
+                database.Add(paramsToAdd[i]);
+            }
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                database.Add(errorParam);
+            });
+        }
+
+        [TestCase(new int[] { 10, 3, 5, 10 }, new int[] { 1, 2, 3, 4, 5 }, 3, 5)]
+        [TestCase(new int[0], new int[] { 1, 2, 3, 4, 5 }, 5, 0)]
+        [TestCase(new int[1] {1}, new int[] { 1}, 1, 1) ]
+        public void Remove_With_ValidData_Positive_Test(int[] ctorParams, int[] paramsToAdd, int removeCount, int expectedCount)
+        {
+            Database database = new Database(ctorParams);
+
+            for (int i = 0; i < paramsToAdd.Length; i++)
+            {
+                database.Add(paramsToAdd[i]);
+            }
+
+            for (int i = 0; i < removeCount; i++)
+            {
+                database.Remove();
             }
 
             Assert.AreEqual(expectedCount, database.Count);
