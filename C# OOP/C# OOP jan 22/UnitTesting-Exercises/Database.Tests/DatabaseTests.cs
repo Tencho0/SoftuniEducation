9b1ffc0ca3ebfb6a0ae3bfb967a9567ecf528b18
Tencho0 <tencho1011@gmail.com>
2022-05-14 23:28:29 +0300

    finished project Database.Tests

--- C# OOP/C# OOP jan 22/UnitTesting-Exercises/Database.Tests/DatabaseTests.cs
+++ C# OOP/C# OOP jan 22/UnitTesting-Exercises/Database.Tests/DatabaseTests.cs
@@ -48,9 +48,9 @@ namespace Database.Tests
             });
         }
 
-        [TestCase(new int[] { 10, 3, 5, 10 }, new int[] { 1, 2, 3, 4, 5 }, 3, 5)]
+        [TestCase(new int[] { 10, 3, 5, 10 }, new int[] { 1, 2, 3, 4, 5 }, 3, 6)]
         [TestCase(new int[0], new int[] { 1, 2, 3, 4, 5 }, 5, 0)]
-        [TestCase(new int[1] {1}, new int[] { 1}, 1, 1) ]
+        [TestCase(new int[1] { 1 }, new int[] { 1 }, 1, 1)]
         public void Remove_With_ValidData_Positive_Test(int[] ctorParams, int[] paramsToAdd, int removeCount, int expectedCount)
         {
             Database database = new Database(ctorParams);
@@ -66,6 +66,66 @@ namespace Database.Tests
             }
 
             Assert.AreEqual(expectedCount, database.Count);
+        }
+
+        [TestCase(new int[] { 10, 3, 5, 10 }, new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[0], new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[1] { 1 }, new int[] { 1 })]
+        public void Remove_With_InvalidData_Negative_Test(int[] ctorParams, int[] paramsToAdd)
+        {
+            Database database = new Database(ctorParams);
+
+            for (int i = 0; i < paramsToAdd.Length; i++)
+            {
+                database.Add(paramsToAdd[i]);
+            }
+
+            while (database.Count > 0)
+            {
+                database.Remove();
+            }
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                database.Remove();
+            });
+        }
+
+
+        [TestCase(new int[] { 10, 3, 5, 10 })]
+        [TestCase(new int[0])]
+        [TestCase(new int[1] { 1 })]
+        public void Fetch(int[] ctorParams)
+        {
+            Database database = new Database(ctorParams);
+
+            int[] items = database.Fetch();
+
+            Assert.AreEqual(items.Length, database.Count);
+        }
+
+
+
+        [TestCase(new int[] { 10, 3, 5, 10 }, new int[] { 1, 2, 3, 4, 5 }, 2, new int[] { 10, 3, 5, 10, 1, 2, 3 })]
+        [TestCase(new int[0], new int[] { 1, 2, 3, 4, 5 }, 5, new int[0])]
+        [TestCase(new int[1] { 1 }, new int[] { 1 }, 1, new int[] {1})]
+        public void Fetch_Positive(int[] ctorParams, int[] paramsToAdd, int removesCount, int[] expectedArray)
+        {
+            Database database = new Database(ctorParams);
+
+            foreach (var item in paramsToAdd)
+            {
+                database.Add(item);
+            }
+
+            for (int i = 0; i < removesCount; i++)
+            {
+                database.Remove();
+            }
+
+            int[] actualData = database.Fetch();
+
+            CollectionAssert.AreEqual(expectedArray, actualData);
         }
     }
 }
