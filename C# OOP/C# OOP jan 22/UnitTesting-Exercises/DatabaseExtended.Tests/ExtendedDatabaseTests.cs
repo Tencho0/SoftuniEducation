f3c95b023dc0786d5a808cbb4fc0f7eb76265884
Tencho0 <tencho1011@gmail.com>
2022-05-15 23:03:12 +0300

    added new project - DatabaseExtended

--- C# OOP/C# OOP jan 22/UnitTesting-Exercises/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
+++ C# OOP/C# OOP jan 22/UnitTesting-Exercises/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
@@ -1,10 +1,28 @@
 namespace DatabaseExtended.Tests
 {
+    using ExtendedDatabase;
     using NUnit.Framework;
+    using System.Collections.Generic;
 
     [TestFixture]
     public class ExtendedDatabaseTests
     {
+        [TestCaseData("TestCaseSourceData")]
+        public void Constructor_Should_Create_Database_Positive_Test(Person[] people, int expectedCount)
+        {
+            Database database = new Database(people);
+            Assert.AreEqual(expectedCount, database.Count);
+        }
 
+        public static IEnumerable<TestCaseData> TestCaseSourceData()
+        {
+            yield return 
+                new TestCaseData(new Person[]
+            {
+                new Person(1, "kircho"), 
+                new Person(2, "ivcho"),
+                new Person(3, "mircho")
+            }, 3);
+        }
     }
 }
