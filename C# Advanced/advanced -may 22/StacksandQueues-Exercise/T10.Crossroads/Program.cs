b05743884309d36a29899ae5f49a61a9ac57aaa7
Tencho0 <tencho1011@gmail.com>
2022-05-21 23:18:48 +0300

    added new tasks in solution StacksandQueues-Exercise

--- C# Advanced/advanced -may 22/StacksandQueues-Exercise/T10.Crossroads/Program.cs
+++ C# Advanced/advanced -may 22/StacksandQueues-Exercise/T10.Crossroads/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace T10.Crossroads
 {
@@ -6,7 +7,56 @@ namespace T10.Crossroads
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Queue<string> cars = new Queue<string>();
+
+            int durationOfGreen = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+           // int totalSecs = 0;
+            int passedCarsCount = 0;
+
+            while (command != "END")
+            {
+
+                if (command != "green")
+                {
+                    cars.Enqueue(command);
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                int currGreenLight = durationOfGreen;
+
+                while (currGreenLight > 0 && cars.Count > 0)
+                {
+                    string currCar = cars.Dequeue();
+                    if (currGreenLight - currCar.Length >= 0)
+                    {
+                        currGreenLight -= currCar.Length;
+                        passedCarsCount++;
+                        continue;
+                    }
+
+                    if (currGreenLight + freeWindow - currCar.Length >= 0)
+                    {
+                        currGreenLight = 0;
+                        passedCarsCount++;
+                        continue;
+                    }
+
+                    int hittetChar =currGreenLight + freeWindow;
+
+                    Console.WriteLine($"A crash happened!");
+                    Console.WriteLine($"{currCar} was hit at {currCar[hittetChar]}.");
+                    Environment.Exit(0);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{passedCarsCount} total cars passed the crossroads.");
+
         }
     }
 }
---
+++ C# Advanced/advanced -may 22/StacksandQueues-Exercise/T11.KeyRevolver/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T11.KeyRevolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = int.Parse(Console.ReadLine());
+            int sizeOfBarrel = int.Parse(Console.ReadLine());
+            int[] bulletsArray = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] locksArray = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int intelligence = int.Parse(Console.ReadLine());
+
+            Stack<int> bullets = new Stack<int>(bulletsArray);
+            Queue<int> locks = new Queue<int>(locksArray);
+
+            int currShots = sizeOfBarrel;
+            int bulletsCount = 0;
+
+            while (bullets.Count > 0 && locks.Count > 0)
+            {
+                int currBullet = bullets.Pop();
+                int currLock = locks.Peek();
+
+                currShots--;
+                bulletsCount++;
+
+                if (currBullet <= currLock)
+                {
+                    Console.WriteLine($"Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                if (currShots <= 0 && bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                    currShots = sizeOfBarrel;
+                }
+            }
+            if (bullets.Count == 0 && locks.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else
+            {
+                int result = intelligence - (bulletsCount * price);
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${result}");
+            }
+        }
+    }
+}
