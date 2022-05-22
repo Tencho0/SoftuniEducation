bed3b71627d87d6fae6f4da36d36195db9510dd7
Tencho0 <tencho1011@gmail.com>
2022-05-22 18:15:30 +0300

    added new tasks in solution StacksandQueues-Exercise

--- C# Advanced/advanced -may 22/StacksandQueues-Exercise/T12.CupsandBottles/Program.cs
+++ C# Advanced/advanced -may 22/StacksandQueues-Exercise/T12.CupsandBottles/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace T12.CupsandBottles
 {
@@ -6,7 +8,35 @@ namespace T12.CupsandBottles
     {
         static void Main(string[] args)
         {
+            int[] cupsArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] bottlesArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
+            Queue<int> cups = new Queue<int>(cupsArr);
+            Stack<int> bottles = new Stack<int>(bottlesArr);
+
+            int wastedWater = 0;
+
+            while (bottles.Count > 0 && cups.Count > 0)
+            {
+                int currBottle = bottles.Pop();
+                int currCup = cups.Peek();
+
+                if (currBottle >= currCup)
+                {
+                    wastedWater += (currBottle - currCup);
+                    cups.Dequeue();
+                }
+                else
+                {
+                    int[] currCups = cups.ToArray();
+                    currCups[0] -= currBottle;
+                    cups = new Queue<int>(currCups);
+                }
+            }
+
+            if (bottles.Count > 0) Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+            else Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
         }
     }
 }
