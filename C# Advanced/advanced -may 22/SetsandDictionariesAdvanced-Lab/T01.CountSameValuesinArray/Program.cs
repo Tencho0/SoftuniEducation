231184ed91c31bb4dbf41ce90333b8b4a3721dd9
Tencho0 <tencho1011@gmail.com>
2022-05-23 21:29:23 +0300

    created new solution - SetsandDictionariesAdvanced-Lab

---
+++ C# Advanced/advanced -may 22/SetsandDictionariesAdvanced-Lab/T01.CountSameValuesinArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01.CountSameValuesinArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> dictionary = new Dictionary<double, int>();
+
+            foreach (var item in arr)
+            {
+                if (!dictionary.ContainsKey(item))
+                {
+                    dictionary[item] = 0;
+                }
+                dictionary[item]++;
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
