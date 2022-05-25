6cd865b3e0b4f59f0e1bd670e61cc1a72f84737d
Tencho0 <tencho1011@gmail.com>
2022-05-25 11:55:33 +0300

    added new tasks in solution - SetsandDictionariesAdvanced-Lab

---
+++ C# Advanced/advanced -may 22/SetsandDictionariesAdvanced-Lab/T07.ParkingLot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] givenCmd = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string action = givenCmd[0];
+                string car = givenCmd[1];
+
+                if (action == "IN")
+                    cars.Add(car);
+                else if (action == "OUT")
+                    cars.Remove(car);
+
+                command = Console.ReadLine();
+            }
+
+            if(cars.Count == 0)
+                Console.WriteLine("Parking Lot is Empty");
+            else
+                Console.WriteLine(string.Join("\n", cars));
+        }
+    }
+}
