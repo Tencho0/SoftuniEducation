accedf4e9066709bf4e2e4d8d09ac2d5051159f7
Tencho0 <tencho1011@gmail.com>
2022-05-28 22:20:35 +0300

    created new solution - Streams,FilesandDirectories-Exercises

---
+++ C# Advanced/advanced -may 22/Streams,FilesandDirectories-Exercises/EvenLines/EvenLines.cs
@@ -0,0 +1,53 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+            StringBuilder sb = new StringBuilder();
+            using (reader)
+            {
+                int index = 0;
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (index % 2 == 0)
+                    {
+                        line = Replace(line);
+                        line = Reverse(line);
+                        sb.AppendLine(line);
+                    }
+                    index++;
+                    line = reader.ReadLine();
+                }
+            }
+            return sb.ToString().Trim();
+        }
+
+        private static string Reverse(string line)
+        {
+            return string.Join(" ", line.Split(' ').Reverse());
+        }
+
+        private static string Replace(string line)
+        {
+            return line.Replace('-', '@')
+                .Replace(',', '@')
+                .Replace('.', '@')
+                .Replace('!', '@')
+                .Replace('?', '@');
+        }
+    }
+}
---
+++ C# Advanced/advanced -may 22/Streams,FilesandDirectories-Exercises/LineNumbers/LineNumbers.cs
@@ -0,0 +1,58 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int count = 0;
+            List<string> allLines = new List<string>();
+            foreach (var line in lines)
+            {
+                count++;
+                int lettersCount = line.Count(x => char.IsLetter(x));
+                int punctuationCount = line.Count(x => char.IsPunctuation(x));
+                string modifiedLine = $"Line {count}: {line} ({lettersCount})({punctuationCount})";
+                allLines.Add(modifiedLine);
+            }
+            File.WriteAllLines(outputFilePath, allLines);
+
+
+            // StreamReader reader = new StreamReader(inputFilePath);
+            // StreamWriter writer = new StreamWriter(outputFilePath);
+            // using (reader)
+            // {
+            //     using (writer)
+            //     {
+            //         int index = 1;
+            //         string line = reader.ReadLine();
+            //
+            //         int lettersCount = 0;
+            //         int punctuationCount = 0;
+            //
+            //         while (line != null)
+            //         {
+            //             lettersCount = line.Where(x => char.IsLetter(x)).Count();
+            //             punctuationCount = line.Where(x => char.IsPunctuation(x)).Count();
+            //             writer.WriteLine($"Line {index}: {line} ({lettersCount})({punctuationCount})");
+            //             index++;
+            //             line = reader.ReadLine();
+            //         }
+            //     }
+            // }
+        }
+    }
+}
