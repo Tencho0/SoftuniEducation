64bf06e112267331cf5f5781e15ba5196aeea759
Tencho0 <tencho1011@gmail.com>
2022-05-30 23:13:00 +0300

    created new solution - FunctionalProgramming-Lab

---
+++ C# Advanced/advanced -may 22/FunctionalProgramming-Lab/T01.SortEvenNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace T01.SortEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+               string.Join(", ",
+               Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(x => x % 2 == 0)
+                .OrderBy(x=>x)
+                ));
+        }
+    }
+}
---
+++ C# Advanced/advanced -may 22/FunctionalProgramming-Lab/T02.SumNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T02.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
