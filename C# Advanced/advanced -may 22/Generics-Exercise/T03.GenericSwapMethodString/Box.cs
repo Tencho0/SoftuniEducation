001f82ce6bd0ff80fbc788b02bbfde72c99f65d7
Tencho0 <tencho1011@gmail.com>
2022-06-10 23:41:57 +0300

    added new tasks in solution Generics-Exercise

---
+++ C# Advanced/advanced -may 22/Generics-Exercise/T03.GenericSwapMethodString/Box.cs
@@ -0,0 +1,15 @@
+﻿namespace T03.GenericSwapMethodString
+{
+    public class Box<T>
+    {
+        public Box(T element)
+        {
+            Element = element;
+        }
+        public T Element{ get; }
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Element}";
+        }
+    }
+}
---
+++ C# Advanced/advanced -may 22/Generics-Exercise/T03.GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T03.GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int cmd = int.Parse(Console.ReadLine());
+                var box = new Box<int>(cmd);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
