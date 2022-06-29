b224f0553bc3e30ff6e56569efd15f6353126c37
Tencho0 <tencho1011@gmail.com>
2022-06-29 21:45:03 +0300

    added new project in solution UnitTesting-Exercises

--- C# OOP/C# OOP jan 22/UnitTesting-Exercises/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
+++ C# OOP/C# OOP jan 22/UnitTesting-Exercises/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
@@ -7,22 +7,79 @@ namespace DatabaseExtended.Tests
     [TestFixture]
     public class ExtendedDatabaseTests
     {
-        [TestCaseData("TestCaseSourceData")]
+        [TestCaseSource("TestCaseSourceData")]
         public void Constructor_Should_Create_Database_Positive_Test(Person[] people, int expectedCount)
         {
             Database database = new Database(people);
             Assert.AreEqual(expectedCount, database.Count);
         }
 
+        [TestCaseSource("TestCaseAddData")]
+        public void Add_Should_Create_Add_Positive_Test
+            (Person[] peopleClor,
+            Person[] peopleAdd,
+            int expectedCount)
+        {
+            Database database = new Database(peopleClor);
+            foreach (var person in peopleAdd)
+            {
+                database.Add(person);
+            }
+            Assert.AreEqual(expectedCount, database.Count);
+        }
+        public static IEnumerable<TestCaseData> TestCaseAddData()
+        {
+            TestCaseData[] testCases = new TestCaseData[]
+            {
+                 new TestCaseData(
+                    new Person[]
+                     {
+                         new Person(1, "kircho"),
+                         new Person(2, "ivcho"),
+                         new Person(3, "mircho"),
+                     },
+                    new Person[]
+                     {
+                         new Person(4, "kircho2"),
+                         new Person(5, "ivcho2"),
+                         new Person(6, "mircho2"),
+                     }, 6),
+                new TestCaseData(
+                    new Person[]
+                     {
+                     },
+                    new Person[]
+                     {
+                         new Person(1, "kircho"),
+                         new Person(2, "ivcho"),
+                         new Person(3, "mircho"),
+                     },
+                    3)
+            };
+
+            foreach (var item in testCases)
+                yield return item;
+        }
+
         public static IEnumerable<TestCaseData> TestCaseSourceData()
         {
-            yield return 
-                new TestCaseData(new Person[]
+            TestCaseData[] testCases = new TestCaseData[]
             {
-                new Person(1, "kircho"), 
-                new Person(2, "ivcho"),
-                new Person(3, "mircho")
-            }, 3);
+                new TestCaseData(new Person[]
+                     {
+                         new Person(1, "kircho"),
+                         new Person(2, "ivcho"),
+                         new Person(3, "mircho"),
+                     },
+                     3),
+                new TestCaseData(new Person[]
+                     {
+                     },
+                     0)
+            };
+
+            foreach (var item in testCases)
+                yield return item;
         }
     }
 }
