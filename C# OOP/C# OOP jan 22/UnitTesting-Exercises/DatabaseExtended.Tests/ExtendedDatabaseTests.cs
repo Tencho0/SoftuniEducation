3d3a9c522895bf240a6aa589a24df7aed0f9696b
Tencho0 <tencho1011@gmail.com>
2022-06-30 23:50:20 +0300

    added new projects in solution - UnitTesting-Exercises

--- C# OOP/C# OOP jan 22/UnitTesting-Exercises/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
+++ C# OOP/C# OOP jan 22/UnitTesting-Exercises/DatabaseExtended.Tests/ExtendedDatabaseTests.cs
@@ -2,6 +2,7 @@ namespace DatabaseExtended.Tests
 {
     using ExtendedDatabase;
     using NUnit.Framework;
+    using System;
     using System.Collections.Generic;
 
     [TestFixture]
@@ -26,6 +27,117 @@ namespace DatabaseExtended.Tests
                 database.Add(person);
             }
             Assert.AreEqual(expectedCount, database.Count);
+        }
+        [TestCaseSource("TestCaseAddInvalidData")]
+        public void Add_ShouldThrow_InvalidOperationException_With_InvalidData_Negative_Test
+            (Person[] peopleClor,
+            Person[] peopleAdd,
+            Person inPerson)
+        {
+            Database database = new Database(peopleClor);
+
+            foreach (var item in peopleAdd)
+            {
+                database.Add(item);
+            }
+            Assert.Throws<InvalidOperationException>(() => database.Add(inPerson));
+        }
+        [TestCaseSource("TestCaseRemoveData")]
+        public void Remove_Should_Remove_With_Invalid_Data
+            (Person[] peopleClor,
+            Person[] peopleAdd,
+            int peopleToRemove,
+            int expectedPeopleCount)
+        {
+            Database database = new Database(peopleClor);
+
+            foreach (var item in peopleAdd)
+                database.Add(item);
+
+            for (int i = 0; i < peopleToRemove; i++)
+                database.Remove();
+            
+            Assert.AreEqual(expectedPeopleCount, database.Count);
+
+        }
+        public static IEnumerable<TestCaseData> TestCaseRemoveData()
+        {
+            TestCaseData[] testCases = new TestCaseData[]
+            {
+                new TestCaseData(
+                    new Person[]
+                     {
+                         new Person(1, "kircho"),
+                         new Person(2, "ivcho"),
+                         new Person(3, "mircho"),
+                     },
+                    new Person[]
+                     {
+                         new Person(4, "kircho2"),
+                         new Person(5, "ivcho2"),
+                         new Person(6, "mircho2"),
+                     },
+                     3,
+                     3)
+            };
+
+            foreach (var item in testCases)
+                yield return item;
+        }
+        public static IEnumerable<TestCaseData> TestCaseAddInvalidData()
+        {
+            TestCaseData[] testCases = new TestCaseData[]
+            {
+                 new TestCaseData(
+                    new Person[]
+                     {
+                         new Person(1, "kircho"),
+                         new Person(2, "ivcho"),
+                         new Person(3, "mircho"),
+                         new Person(4, "mircho2"),
+                         new Person(5, "mircho3"),
+                         new Person(6, "mircho4"),
+                         new Person(7, "mircho5"),
+                         new Person(8, "mircho6"),
+                         new Person(9, "mircho7"),
+                         new Person(10, "mircho8"),
+                         new Person(11, "mircho9"),
+                         new Person(12, "mircho10"),
+                         new Person(13, "mircho11"),
+                     },
+                    new Person[]
+                     {
+                         new Person(14, "kircho2"),
+                         new Person(15, "ivcho2"),
+                         new Person(16, "mircho12"),
+                     },
+                    new Person(17, "kircho33")),
+                new TestCaseData(
+                    new Person[]
+                     {
+                     },
+                    new Person[]
+                     {
+                         new Person(1, "kircho"),
+                         new Person(2, "ivcho"),
+                         new Person(3, "mircho"),
+                     },
+                    new Person(4, "kircho")),
+                new TestCaseData(
+                    new Person[]
+                     {
+                     },
+                    new Person[]
+                     {
+                         new Person(1, "kircho"),
+                         new Person(2, "ivcho"),
+                         new Person(3, "mircho"),
+                     },
+                    new Person(3, "kircho1111"))
+            };
+
+            foreach (var item in testCases)
+                yield return item;
         }
         public static IEnumerable<TestCaseData> TestCaseAddData()
         {
@@ -60,7 +172,6 @@ namespace DatabaseExtended.Tests
             foreach (var item in testCases)
                 yield return item;
         }
-
         public static IEnumerable<TestCaseData> TestCaseSourceData()
         {
             TestCaseData[] testCases = new TestCaseData[]
--- C# OOP/C# OOP jan 22/UnitTesting-Exercises/FightingArena.Tests/WarriorTests.cs
+++ C# OOP/C# OOP jan 22/UnitTesting-Exercises/FightingArena.Tests/WarriorTests.cs
@@ -1,10 +1,88 @@
 namespace FightingArena.Tests
 {
     using NUnit.Framework;
+    using System;
 
     [TestFixture]
     public class WarriorTests
     {
+        [Test]
+        public void Constructor_Should_Test_DataProterty()
+        {
+            Warrior warrior = new Warrior("qnko", 100, 100);
+            Assert.AreEqual("qnko", warrior.Name);
+            Assert.AreEqual(100, warrior.HP);
+            Assert.AreEqual(100, warrior.Damage);
 
+            warrior = new Warrior("dqnko", 1, 0);
+            Assert.AreEqual("dqnko", warrior.Name);
+            Assert.AreEqual(0, warrior.HP);
+            Assert.AreEqual(1, warrior.Damage);
+        }
+        [Test]
+        public void Constructor_Should_ThrowException_InvalidData()
+        {
+            Assert.Throws<ArgumentException>(
+                () => new Warrior(null, 100, 100));
+
+            Assert.Throws<ArgumentException>(
+                () => new Warrior(" ", 100, 100));
+
+            Assert.Throws<ArgumentException>(
+                () => new Warrior("", 100, 100));
+
+            Assert.Throws<ArgumentException>(
+                () => new Warrior("qnko", 0, 100));
+
+            Assert.Throws<ArgumentException>(
+                () => new Warrior("qnko", -1, 100));
+
+            Assert.Throws<ArgumentException>(
+                () => new Warrior("qnko", 50, -1));
+        }
+        [Test]
+        public void Fight_Should_Throw_Exception_InvalidData()
+        {
+            Warrior attacker = new Warrior("Stoyan", 50, 5);
+            Warrior deffender = new Warrior("Kiro", 50, 50);
+            Assert.Throws<InvalidOperationException>(
+                () => attacker.Attack(deffender));
+
+            attacker = new Warrior("Stoyan", 50, 100);
+            deffender = new Warrior("Kiro", 50, 5);
+            Assert.Throws<InvalidOperationException>(
+                () => attacker.Attack(deffender));
+
+            attacker = new Warrior("Stoyan", 50, 50);
+            deffender = new Warrior("Kiro", 100, 100);
+            Assert.Throws<InvalidOperationException>(
+                () => attacker.Attack(deffender));
+        }
+        [Test]
+        public void Fight_Should_Fight_With_ValidData()
+        {
+            Warrior attacker = new Warrior("Stoyan", 101, 100);
+            Warrior deffender = new Warrior("Kiro", 60, 100);
+
+            attacker.Attack(deffender);
+            
+            Assert.AreEqual(0,
+                deffender.HP); 
+
+            Assert.AreEqual(40,
+                attacker.HP);
+
+
+            attacker = new Warrior("Stoyan", 50, 100);
+            deffender = new Warrior("Kiro", 50, 100);
+
+            attacker.Attack(deffender);
+            
+            Assert.AreEqual(50,
+                deffender.HP); 
+
+            Assert.AreEqual(50,
+                attacker.HP);
+        }
     }
 }
