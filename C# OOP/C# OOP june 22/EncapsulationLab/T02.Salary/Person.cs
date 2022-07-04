3c2c966db0282dd4229d8babe57d211e9c990463
Tencho0 <tencho1011@gmail.com>
2022-07-04 22:37:18 +0300

    created new solution - EncapsulationLab

---
+++ C# OOP/C# OOP june 22/EncapsulationLab/T02.Salary/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            private set { lastName = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+        public decimal Salary
+        {
+            get { return salary; }
+            private set { salary = value; }
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+            //if (this.Age < 30)
+            //{
+            //    this.Salary += (this.Salary * percentage) / 200;
+            //}
+            //else
+            //{
+            //    this.Salary += this.Salary * percentage / 100;
+            //}
+            if (this.Age < 30)
+                percentage /= 2.0m;
+            this.Salary *= (1 + percentage / 100.0m);
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+        }
+    }
+}
