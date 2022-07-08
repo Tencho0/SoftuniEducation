1393ff11096090a7b9c81881ff6ceed6baff3ef7
Tencho0 <tencho1011@gmail.com>
2022-07-08 22:46:28 +0300

    created new solution - InterfacesandAbstractionLab

---
+++ C# OOP/C# OOP june 22/InterfacesandAbstractionLab/T02.Cars/Tesla.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get ; set; }
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+        public override string ToString()
+        {
+            return $"{this.Color} {this.Model} with {this.Battery} Batteries\n{this.Start()}\n{this.Stop()}";
+        }
+    }
+}
