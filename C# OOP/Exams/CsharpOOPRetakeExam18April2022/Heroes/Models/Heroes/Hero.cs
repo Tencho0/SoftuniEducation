82c53a6bb1d0bfdcf962f4fcbba72a315365e86d
Tencho0 <tencho1011@gmail.com>
2022-07-30 22:29:42 +0300

    created new solution - CsharpOOPRetakeExam18April2022

---
+++ C# OOP/Exams/CsharpOOPRetakeExam18April2022/Heroes/Models/Heroes/Hero.cs
@@ -0,0 +1,87 @@
+﻿namespace Heroes.Models.Heroes
+{
+    using global::Heroes.Models.Contracts;
+    using System;
+
+    public abstract class Hero : IHero
+    {
+        private string name;
+        private int health;
+        private int armour;
+        private IWeapon weapon;
+
+        public Hero(string name, int health, int armour)
+        {
+            Name = name;
+            Health = health;
+            Armour = armour;
+        }
+
+        public bool IsAlive => this.Health > 0;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException($"Hero name cannot be null or empty.");
+
+                name = value;
+            }
+        }
+        public int Health
+        {
+            get { return health; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException($"Hero health cannot be below 0.");
+
+                health = value;
+            }
+        }
+        public int Armour
+        {
+            get { return armour; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException($"Hero armour cannot be below 0.");
+
+                armour = value;
+            }
+        }
+        public IWeapon Weapon
+        {
+            get { return weapon; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException($"Weapon cannot be null.");
+
+                weapon = value;
+            }
+        }
+
+        public void AddWeapon(IWeapon weapon)
+        {
+            this.Weapon = weapon;
+        }
+
+        public void TakeDamage(int points)
+        {
+            if (this.Armour - points >= 0)
+            {
+                this.Armour -= points;
+            }
+            else if (this.Armour - points < 0)
+            {
+                this.Health -= (points - this.Armour);
+                this.Armour = 0;
+            }
+
+            if (this.Health < 0)
+                this.Health = 0;
+        }
+    }
+}
---
+++ C# OOP/Exams/CsharpOOPRetakeExam18April2022/Heroes/Models/Weapons/Weapon.cs
@@ -0,0 +1,34 @@
+﻿namespace Heroes.Models.Weapons
+{
+    using global::Heroes.Models.Contracts;
+    using System;
+
+    public abstract class Weapon : IWeapon
+    {
+        private string name;
+        private int durability;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException($"Weapon type cannot be null or empty.");
+
+                name = value;
+            }
+        }
+        public int Durability
+        {
+            get { return durability; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException($"Durability cannot be below 0.");
+                durability = value;
+            }
+        }
+        public abstract int DoDamage();
+    }
+}
