730a6c38d817a12888fb7cf20c4fe26ca45b6009
Tencho0 <101017140+Tencho0@users.noreply.github.com>
2022-10-20 19:06:34 +0300

    Update solution.js

--- JS Advanced/JS Advanced - Jan 2022/AdvancedFunctionsExercise/10. Central Cinema/solution.js
+++ JS Advanced/JS Advanced - Jan 2022/AdvancedFunctionsExercise/10. Central Cinema/solution.js
@@ -1,4 +1,96 @@
 function solve() {
+    let movieElement = document.querySelector('#container input:nth-of-type(1)');
+    let hallElement = document.querySelector('#container input:nth-of-type(2)');
+    let priceElement = document.querySelector('#container input:nth-of-type(3)');
+    let onScreenBtn = document.querySelector('#container button');
+    let clearBtn = document.querySelector('#archive button');
+
+    let moviesOnScreen = document.querySelector('#movies ul');
+    let archive = document.querySelector('#archive ul');
+
+    function createElement(type, text) {
+        let result = document.createElement(type);
+        if (text) {
+            result.textContent = text;
+        }
+        return result;
+    }
+
+    onScreenBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let movie = movieElement.value;
+        let hall = hallElement.value;
+        let price = priceElement.value;
+
+        movieElement.value = '';
+        hallElement.value = '';
+        priceElement.value = '';
+
+        if (!movie || !hall) {
+            return;
+        }
+        if (price === '' || Number.isNaN(Number(price))) {
+            return;
+        }
+        price = Number(price);
+
+        let li = createElement('li');
+        let span = createElement('span', movie);
+        let strong = createElement('strong', `Hall: ${hall}`);
+
+        let div = createElement('div');
+        let strongPrice = createElement('strong', price.toFixed(2));
+        let inputSoldTickets = createElement('input');
+        inputSoldTickets.placeholder = 'Tickets Sold';
+        let archiveBtn = createElement('button', 'Archive');
+
+        div.appendChild(strongPrice);
+        div.appendChild(inputSoldTickets);
+        div.appendChild(archiveBtn);
+
+        li.appendChild(span);
+        li.appendChild(strong);
+        li.appendChild(div);
+
+        moviesOnScreen.appendChild(li);       
+
+        archiveBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            if (inputSoldTickets.value === '' || Number.isNaN(Number(inputSoldTickets.value))) {
+                return;
+            }
+
+            li.removeChild(strong);
+            li.removeChild(div);
+
+            let totalPrice = (Number(inputSoldTickets.value) * Number(strongPrice.textContent)).toFixed(2);
+            let archiveStrong = createElement('strong', `Total amount: ${totalPrice}`);
+            let deleteBtn = createElement('button', 'Delete');
+
+            li.appendChild(archiveStrong);
+            li.appendChild(deleteBtn);
+
+            archive.appendChild(li);
+
+            deleteBtn.addEventListener('click', () => {
+                li.remove();
+            })
+
+            clearBtn.addEventListener('click', () => {
+                archive.innerHTML = '';
+            })
+        })
+    })
+}
+
+
+
+
+
+
+function solve() {
     const [name, hall, ticketPrice] = document.querySelectorAll("#container input");
     const movieSection = document.querySelector("#movies ul");
     const archiveSection = document.querySelector("#archive ul");
