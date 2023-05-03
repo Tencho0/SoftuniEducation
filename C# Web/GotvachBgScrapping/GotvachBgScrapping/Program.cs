7e597ee284fd99cb45f1ecddc3edb286917d7776
Tencho <tencho1011@gmail.com>
2023-05-03 23:18:45 +0300

    initialize gotvachbg scrapper

---
+++ C# Web/GotvachBgScrapping/GotvachBgScrapping/Program.cs
@@ -0,0 +1,23 @@
+﻿using AngleSharp.Dom;
+using AngleSharp;
+
+namespace GotvachBgScrapping
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            IConfiguration config = Configuration.Default.WithDefaultLoader();
+            IBrowsingContext context = BrowsingContext.New(config);
+
+            var document = await context.OpenAsync("https://recepti.gotvach.bg/r-160281");
+
+            var elements = document.QuerySelectorAll(".products > ul > li");
+
+            foreach (var item in elements)
+            {
+                Console.WriteLine(item.TextContent);
+            }
+        }
+    }
+}
