659709f8e5389b96b3523b8723dc12319c1199cf
Tencho <tencho1011@gmail.com>
2023-06-09 22:31:48 +0300

    created new app - TaskBoardApp

---
+++ C# Web/TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskBoardApp.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
