54d1a8b69a2066ccf5ed3d16ccad5b82ab40e69c
Tencho <tencho1011@gmail.com>
2023-06-15 22:29:51 +0300

    added new solution

---
+++ HackRank/ProgressTechInterview/ProgressTechInterview/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ProgressTechInterview
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            Result.fizzBuzz(n);
+        }
+    }
+    class Result
+    {
+        public static void fizzBuzz(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 3 != 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+}
---
+++ HackRank/ProgressTechInterview/T1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+class Result
+{
+
+    /*
+     * Complete the 'ValidatePassSentences' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING_ARRAY input as parameter.
+     */
+
+    public static int ValidatePassSentences(List<string> input)
+    {
+        int count = 0;
+        foreach (string passSentence in input)
+        {
+            string[] words = passSentence.Split(' ');
+            HashSet<string> uniqueWords = new HashSet<string>(words);
+            if (words.Length == uniqueWords.Count)
+            {
+                count++;
+            }
+        }
+
+        return count;
+
+
+        //     int count;
+        //     var dict = new List<string>();
+        // foreach (string sentence in input)
+        // {
+        //     bool isValid = true;
+        //     foreach(string word in sentence.Split(' '))
+        //     {
+        //         if(dict.ContainsKey(word))      
+        //         {
+        //             isValid=false;
+        //             break;
+        //         }    
+        //         else
+        //         {
+        //             dict.Add(word);
+        //         }
+        //     }
+        //     if(isValid)
+        //     {
+        //         count++;
+        //     }
+
+        //     dict =  new List<string>();
+        // }  
+        // return count;
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int inputCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> input = new List<string>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                string inputItem = Console.ReadLine();
+                input.Add(inputItem);
+            }
+
+            int result = Result.ValidatePassSentences(input);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
