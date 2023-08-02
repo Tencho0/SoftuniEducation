76a7c3a0ea91aa4e6a825781a18873a98cb64038
Tencho <tencho1011@gmail.com>
2023-08-02 20:54:30 +0300

    Added new tech assignment

---
+++ HackRank/Leetcode/T1.TwoSum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace T1.TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums = { 2, 7, 11, 15 };
+            var target = 9;
+            var result = solution.TwoSum(nums, target);
+            Console.WriteLine($"[{string.Join(", ", result)}]");
+        }
+    }
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (var i = 0; i <= nums.Length - 1; i++)
+            {
+                for (var j = i + 1; j <= nums.Length - 1; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ HackRank/Leetcode/T26RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace T26RemoveDuplicatesFromSortedArray
+{
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+            int k = solution.RemoveDuplicates(nums);
+            Console.WriteLine(k);
+        }
+    }
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int k = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                    dict[nums[i]]++;
+                else if (!dict.ContainsKey(nums[i]))
+                {
+                    k++;
+                    dict.Add(nums[i], 1);
+                }
+            }
+
+            return k;
+        }
+    }
+}
