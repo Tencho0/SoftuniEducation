71d5102e6162920df8ab409a3bf091c3c168a9a1
Tencho <tencho1011@gmail.com>
2023-08-15 22:24:02 +0300

    Added new solution

---
+++ C# Algorithms-Advanced/GraphsBellmanFordLongestPathIn(DAG)/T01BellmanFord/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace T01BellmanFord
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        class Edge
+        {
+            public int From { get; set; }
+            public int To { get; set; }
+            public int Weight { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            var graph = new List<Edge>();
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                graph.Add(new Edge()
+                {
+                    From = edgeData[0],
+                    To = edgeData[1],
+                    Weight = edgeData[2]
+                });
+            }
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            var distance = new double[nodes + 1];
+            Array.Fill(distance, double.PositiveInfinity);
+
+            distance[source] = 0;
+
+            // Array to find the path
+            var parent = new int[nodes + 1];
+            Array.Fill(parent, -1);
+
+            // Start of Belman Ford Algorithm
+            for (int i = 0; i < nodes - 1; i++)
+            {
+                // If update variable is false that means that the algorithm
+                // already found the best path and we can break the loop
+                var update = false;
+
+                foreach (var edge in graph)
+                {
+                    if (double.IsPositiveInfinity(distance[edge.From]))
+                    {
+                        continue;
+                    }
+
+                    var newDistance = distance[edge.From] + edge.Weight;
+
+                    if (newDistance < distance[edge.To])
+                    {
+                        distance[edge.To] = newDistance;
+                        parent[edge.To] = edge.From;
+                        update = true;
+                    }
+                }
+
+                if (!update)
+                {
+                    break;
+                }
+            }
+
+            // One more run of the algorithm so we can find negative loops.
+            foreach (var edge in graph)
+            {
+                var newDistance = distance[edge.From] + edge.Weight;
+
+                if (newDistance < distance[edge.To])
+                {
+                    Console.WriteLine("Negative Cycle Detected");
+                    return;
+                }
+            }
+
+            // Find shortest path
+            var path = new Stack<int>();
+            var node = destination;
+
+            while (node != -1)
+            {
+                path.Push(node);
+                node = parent[node];
+            }
+
+            Console.WriteLine(string.Join(" ", path));
+            Console.WriteLine(distance[destination]);
+        }
+    }
+}
---
+++ C# Algorithms-Advanced/GraphsBellmanFordLongestPathIn(DAG)/T02LongestPath/Program.cs
@@ -0,0 +1,119 @@
+﻿namespace T02LongestPath
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        class Edge
+        {
+            public int From { get; set; }
+            public int To { get; set; }
+            public int Weight { get; set; }
+        }
+
+        private static new Dictionary<int, List<Edge>> graph;
+
+        static void Main(string[] args)
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            graph = new Dictionary<int, List<Edge>>();
+
+            ReadGraph(edges);
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            // For longest path use "Negative Infinity"
+            var distance = new double[nodes + 1];
+            Array.Fill(distance, double.NegativeInfinity);
+
+            distance[source] = 0;
+
+            var sortedNodes = TopologicalSorting(graph);
+
+            while (sortedNodes.Count > 0)
+            {
+                var node = sortedNodes.Pop();
+
+                foreach (var edge in graph[node])
+                {
+                    var newDistance = distance[edge.From] + edge.Weight;
+
+                    // For longest path heck if new distance is higher than the curent
+                    if (newDistance > distance[edge.To])
+                    {
+                        distance[edge.To] = newDistance;
+                    }
+                }
+            }
+
+            Console.WriteLine(distance[destination]);
+        }
+
+        private static void ReadGraph(int edges)
+        {
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var from = edgeData[0];
+                var to = edgeData[1];
+
+                if (!graph.ContainsKey(from))
+                {
+                    graph[from] = new List<Edge>();
+                }
+
+                if (!graph.ContainsKey(to))
+                {
+                    graph[to] = new List<Edge>();
+                }
+
+                graph[from].Add(new Edge()
+                {
+                    From = from,
+                    To = to,
+                    Weight = edgeData[2]
+                });
+            }
+        }
+
+        private static Stack<int> TopologicalSorting(Dictionary<int, List<Edge>> graph)
+        {
+            var result = new Stack<int>();
+
+            var visisted = new HashSet<int>();
+
+            foreach (var node in graph.Keys)
+            {
+                DFS(node, result, visisted);
+            }
+
+            return result;
+        }
+
+        private static void DFS(int node, Stack<int> result, HashSet<int> visited)
+        {
+            if (visited.Contains(node))
+            {
+                return;
+            }
+
+            visited.Add(node);
+
+            foreach (var edge in graph[node])
+            {
+                DFS(edge.To, result, visited);
+            }
+
+            result.Push(node);
+        }
+    }
+}
